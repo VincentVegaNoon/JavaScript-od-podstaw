5539725520ef6e4b8a21f65b2f52c19aedfea453
VincentVegaNoon <jan.andrzej.owczarczyk@gmail.com>
2022-10-29 23:45:50 +0200

    js

---
+++ Day 6/Projekt 3/main.js
@@ -0,0 +1,21 @@
+const names = [];
+const div = document.querySelector('div');
+
+const addName = (e) => {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    const newName = input.value;
+    if (input.value.length) {
+        for(name of names) {
+            if(name === newName) {
+                alert('to juz jest')
+                return
+            }
+        }
+    names.push(newName);
+    console.log(names);
+    div.textContent += newName + ', ';
+    input.value = '';
+}
+}
+document.querySelector('button').addEventListener('click', addName);
